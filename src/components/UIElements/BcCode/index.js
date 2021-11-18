0989dec51214e5b2a0a69002b6a33ccb70cbcf07
Walter <wakyj07@gmail.com>
2021-11-19 01:32:23 +0300

    Delete obsolete comments

--- src/components/UIElements/BcCode/index.js
+++ src/components/UIElements/BcCode/index.js
@@ -8,8 +8,7 @@ function BcCode(values) {
 }
 
 BcCode.label = 'Code';
-// BcCode.icon = '<svg viewBox="0 0 24 24"><path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/></svg>';
-BcCode.icon = '<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="16 18 22 12 16 6"></polyline><polyline points="8 6 2 12 8 18"></polyline></svg>';
+BcCode.icon = '<svg viewBox="0 0 24 24"><path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/></svg>';
 
 BcCode.props = {
     language: {
@@ -36,10 +35,6 @@ BcCode.props = {
 }
 
 BcCode.doRender = function (options, preview) {
-    // console.log("Highlighter: ", hljs);
-    // const code = options.code;
-    // const margin = preview ? '' : 'padding: 0 2rem';
-
     const code = hljs.highlight(options.language, options.code).value;
     const margin = preview ? 'margin: -2rem' : 'padding: 0 2rem';
     const colors = options.theme == 'Dark' ? 'background: #353030; color: white;' : 'background: #f5f5f5;';
